9d026d2e3a3e60f5cb23ac4527dcbc742671b30a
LivingBrain <pawel.borkowski@hotmail.com>
2020-05-27 17:44:04 +0200

    Zadanie 2.3

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -8,27 +8,25 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Napisz program, który oblicza deltę dla równania kwadratowego ax^2 + bx^2 + x = 0
-             * Program ma prosić użytkownika o podanie współczynników równania a, b oraz c. Wzór: 
-             * delta = b^2 - 4ac
+             * Napisz program, który oblicza wskaźnik masy ciała BMI. Program ma prosić 
+             * użytkownika o podanie wagi w kilogramach oraz wzrostu w metrach. Wzór: 
+             * BMI = masa / wzrost^2
              */
 
-            Console.WriteLine("Delta calculator.");
-            Console.Write("Provide value a: ");
-            var aValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
-            Console.Write("Provide value b: ");
-            var bValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
-            Console.Write("Provide value c: ");
-            var cValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
+            Console.WriteLine("BMI calculator.");
+            Console.Write("Provide mass value in kg: ");
+            var massValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
+            Console.Write("Provide height value in m: ");
+            var heightValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
             
-            Console.WriteLine($"Calculated delta: {DeltaCalculator(aValue, bValue, cValue)}");
+            Console.WriteLine($"Calculated delta: {BmiCalculator(massValue, heightValue):0.##}");
             Console.ReadKey();
         }
 
-        private static double DeltaCalculator(double aValue, double bValue, double cValue)
+        private static double BmiCalculator(double massValue, double heightValue)
         {
-            var calculatedDelta = Math.Pow(bValue, 2) + 4 * aValue * cValue;
-            return calculatedDelta;
+            var calculatedBmi = massValue / Math.Pow(heightValue, 2);
+            return calculatedBmi;
         }
 
         private static double StringToDecimalParseAndDotReplace(string toConvert)
