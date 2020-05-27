8e3ed892ac361512122370d71dd23898b4c5aeb5
LivingBrain <pawel.borkowski@hotmail.com>
2020-05-27 15:00:59 +0200

    Zadanie 2.1

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RozwiazaneZadania
 {
@@ -10,7 +6,25 @@ namespace RozwiazaneZadania
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            /*
+             * Napisz program przeliczający temperaturę w stopniach Celsjusza na temperaturę w
+             * stopniach Fahrenheita. Program ma prosić użytkownika o podanie temperatury w stopniach
+             * Celsjusza. Wzór: 
+             * F = 32 + (9/5)C
+             */
+
+            Console.WriteLine("Celsius to Fahrenheit calculator.");
+            Console.Write("Provide Celsius value: ");
+            string celsiusValue = Console.ReadLine();
+            celsiusValue = celsiusValue != null && celsiusValue.Contains(".") ? celsiusValue.Replace(".", ",") : celsiusValue;
+            Console.WriteLine($"Fahrenheit: {CalculateCelsiusToFahrenheit(decimal.Parse(celsiusValue ?? throw new InvalidOperationException()))}");
+            Console.ReadKey();
+        }
+
+        private static decimal CalculateCelsiusToFahrenheit(decimal celsiusValue)
+        {
+            var calculatedFahrenheitValue = (celsiusValue * 9 / 5 ) + 32;
+            return calculatedFahrenheitValue;
         }
     }
 }
