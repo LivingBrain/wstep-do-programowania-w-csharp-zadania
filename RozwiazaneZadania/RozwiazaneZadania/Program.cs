d00274dea1cb117b59d2464dc03369d97fd9af06
LivingBrain <pawel.borkowski@hotmail.com>
2020-05-27 17:48:21 +0200

    Zadanie 2.4

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -8,32 +8,22 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Napisz program, który oblicza wskaźnik masy ciała BMI. Program ma prosić 
-             * użytkownika o podanie wagi w kilogramach oraz wzrostu w metrach. Wzór: 
-             * BMI = masa / wzrost^2
+             * Po wykonaniu poniższych linii programu:
+             * int x = 100;
+             * Console.WriteLine(++x * 2);
              */
 
-            Console.WriteLine("BMI calculator.");
-            Console.Write("Provide mass value in kg: ");
-            var massValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
-            Console.Write("Provide height value in m: ");
-            var heightValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
-            
-            Console.WriteLine($"Calculated delta: {BmiCalculator(massValue, heightValue):0.##}");
+            var x = 100;
+            var y = 100;
+
+            Console.WriteLine("Increment operator");
+            Console.WriteLine("int x = 100;");
+            Console.WriteLine("Console.WriteLine(++x * 2);");
+            Console.WriteLine(++x * 2);
+            Console.WriteLine("int y = 100;");
+            Console.WriteLine("Console.WriteLine(y++ * 2);");
+            Console.WriteLine(y++ * 2);
             Console.ReadKey();
-        }
-
-        private static double BmiCalculator(double massValue, double heightValue)
-        {
-            var calculatedBmi = massValue / Math.Pow(heightValue, 2);
-            return calculatedBmi;
-        }
-
-        private static double StringToDecimalParseAndDotReplace(string toConvert)
-        {
-            var dotReplaced = toConvert.Contains(".") ? toConvert.Replace(".", ",") : toConvert;
-            var convertedValue = double.Parse(dotReplaced);
-            return convertedValue;
         }
     }
 }
