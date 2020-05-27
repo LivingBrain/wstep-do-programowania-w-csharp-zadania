f35b542e35834833b11133f4391e3877c96aea60
LivingBrain <pawel.borkowski@hotmail.com>
2020-05-27 18:03:03 +0200

    Zadanie 2.7

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -8,28 +8,21 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Jaką wartość będzie miała zmienna x po wykonaniu poniższych instrukcji? Najpierw
-             * oblicz wartości zmiennych, a dopiero później sprawdź wynik uruchamiając program.
-             * int x, y = 4;
-             * x = (y -= 2);
-             * x = y++;
-             * x = y--; 
+             * Co wyświetli się na ekranie po wykonaniu poniższych instrukcji. Najpierw oblicz 
+             * wartości zmiennych, a dopiero później sprawdź wynik uruchamiając program.
+             *  int x, y = 5;
+             *  x = ++y * 2;
+             *  x = y++;
+             *  x = y--;
+             *  Console.WriteLine(++y); 
              */
 
-            int x, y = 4;
-            Console.WriteLine("int x, y = 4;");
-            x = (y -= 2);
-            Console.WriteLine("x = (y -= 2);");
-            Console.WriteLine(x);
-            Console.WriteLine($"y value = {y}");
+            int x, y = 5;
+            x = ++y * 2;
             x = y++;
-            Console.WriteLine("x = y++;");
-            Console.WriteLine($"y value = {y}");
-            Console.WriteLine($"x value = {x}");
             x = y--;
-            Console.WriteLine("x = y--;");
-            Console.WriteLine($"y value = {y}");
-            Console.WriteLine($"x value = {x}");
+            Console.WriteLine(y);
+            Console.WriteLine(++y);
 
             Console.ReadKey();
         }
