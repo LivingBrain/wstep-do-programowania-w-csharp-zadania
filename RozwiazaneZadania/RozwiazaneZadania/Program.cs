16c9d130887261f22885783e8f12a14a929e7b82
LivingBrain <pawel.borkowski@hotmail.com>
2020-05-27 18:15:20 +0200

    Zadanie 2.9

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -8,19 +8,73 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Po wykonaniu poniższych linii programu: 
-             * bool x;
-             * int y = 1, z = 1;
-             * x = (y == 1 && z++ == 1);
+             * Jaką wartość przyjmą zmienne użyte w programie po wykonaniu poniższych instrukcji? 
+             * Najpierw ustal wartości zmiennych, a dopiero później sprawdź wynik uruchamiając program
+                a)
+                int x = 1, y = 4, z = 2;
+                bool wynik = (x++ > 1 && y++ == 4 && z-- > 0);
+                Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+                b)
+                int x = 1, y = 4, z = 2;
+                bool wynik = (x++ > 1 & y++ == 4 && z-- > 0);
+                Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+                c)
+                int x = 1, y = 4, z = 2;
+                bool wynik = (x++ > 1 & y++ == 4 & z-- > 0);
+                Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+                d)
+                int x = 1, y = 3, z = 4;
+                bool wynik = (x == 1 || y++ > 2 || ++z > 0);
+                Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+                e)
+                int x = 1, y = 3, z = 4;
+                bool wynik = (x == 1 | y++ > 2 || ++z > 0);
+                Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+                f)
+                int x = 1, y = 3, z = 4;
+                bool wynik = (x == 1 | y++ > 2 | ++z > 0);
+                Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z); 
              */
 
-            bool x;
-            int y = 1, z = 1;
-            x = (y == 1 && z++ == 1);
+            Console.WriteLine("a)");
+            int x = 1, y = 4, z = 2;
+            bool wynik = (x++ > 1 && y++ == 4 && z-- > 0);
+            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
 
-            Console.WriteLine($"x value = {x}");
-            Console.WriteLine($"y value = {y}");
-            Console.WriteLine($"z value = {z}");
+            Console.WriteLine("b)");
+            x = 1;
+            y = 4; 
+            z = 2;
+            wynik = (x++ > 1 & y++ == 4 && z-- > 0);
+            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+            Console.WriteLine("c)");
+            x = 1;
+            y = 4; 
+            z = 2;
+            wynik = (x++ > 1 & y++ == 4 & z-- > 0);
+            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+            Console.WriteLine("d)");
+            x = 1;
+            y = 3; 
+            z = 4;
+            wynik = (x == 1 || y++ > 2 || ++z > 0);
+            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+            Console.WriteLine("e)");
+            x = 1;
+            y = 3; 
+            z = 4;
+            wynik = (x == 1 | y++ > 2 || ++z > 0);
+            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+            Console.WriteLine("f)");
+            x = 1;
+            y = 3; 
+            z = 4;
+            wynik = (x == 1 | y++ > 2 | ++z > 0);
+            Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
 
             Console.ReadKey();
         }
