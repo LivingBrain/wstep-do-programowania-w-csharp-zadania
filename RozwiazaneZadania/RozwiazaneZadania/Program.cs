2914e9b28cb09684b5a8af09c62092a57f3cebb7
LivingBrain <realcookie@yahoo.com>
2020-06-02 19:37:15 +0200

    Zadanie 3.7

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -7,44 +7,38 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Pobierz od użytkownika wartość średniej ocen. Program ma wyświetlać informacje o
-             * wysokości przysługującego stypendium zgodnie z poniższą tabelą:
-             * 2,00 3,99 0,00 zł
-             * 4,00 4,79 350,00 zł
-             * 4,80 5,00 550,00 zł
+             * Wykonaj program z przykładu 3.8(str. 63) z użyciem instrukcji switch..case(zamiast if..else).
              */
-
-            Console.WriteLine("Scholarship Amount.");
-            Console.Write("Provide average grade: ");
-            var averageGrade = StringToDecimalParseAndDotReplace(Console.ReadLine());
-            Console.WriteLine($"Scholarship Amount: {ScholarshipAmount(averageGrade)}");
-            Console.ReadKey();
-        }
-
-        private static double StringToDecimalParseAndDotReplace(string toConvert)
-        {
-            var dotReplaced = toConvert.Contains(".") ? toConvert.Replace(".", ",") : toConvert;
-            var convertedValue = double.Parse(dotReplaced);
-            return convertedValue;
-        }
-
-        private static string ScholarshipAmount(double averageGrade)
-        {
-            var scholarshipAmount = 0;
-            switch (averageGrade)
+            Console.Write("Wpisz nr dnia tygodnia: ");
+            string numer = Console.ReadLine();
+            switch (numer)
             {
-                case var i when i >= 2.0 && i <= 3.99:
-                    scholarshipAmount = 0;
+                case "1":
+                    Console.WriteLine("Poniedziałek");
                     break;
-                case var i when i >= 4.0 && i <= 4.79:
-                    scholarshipAmount = 350;
+                case "2":
+                    Console.WriteLine("Wtorek");
                     break;
-                case var i when i >= 4.80 && i <= 5.00:
-                    scholarshipAmount = 550;
+                case "3":
+                    Console.WriteLine("Środa");
                     break;
-            }
-
-            return $"{scholarshipAmount:0.##} zł";
-        }
+                case "4":
+                    Console.WriteLine("Czwartek");
+                    break;
+                case "5":
+                    Console.WriteLine("Piątek");
+                    break;
+                case "6":
+                    Console.WriteLine("Sobota");
+                    break;
+                case "7":
+                    Console.WriteLine("Niedziela");
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiego dnia tygodnia");
+                    break;
+            };
+            Console.ReadKey();
+        }     
     }
 }
