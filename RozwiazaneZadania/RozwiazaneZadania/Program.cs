6aa7686b47dcb47ea094d0860aa8a6ba83968012
LivingBrain <realcookie@yahoo.com>
2020-06-02 19:02:34 +0200

    Zadanie 3.6

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -7,46 +7,81 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Napisz program obliczający liczbę pierwiastków równania kwadratowego. Program ma 
-             * prosić  użytkownika  o  podanie  współczynników  równania,  a  następnie  ma  wyświetlić 
-             * stosowny komunikat. 
+             * Napisz program, który oblicza wskaźnik masy ciała BMI. Program ma prosić 
+             * użytkownika o podanie wagi w kilogramach oraz wzrostu w metrach. Wzór: 
+             * BMI = masa / wzrost^2
+             * 
+             * a)Po  obliczeniu  wskaźnika  BMI  program  powinien  wyświetlać  stosowną  informację  w
+             * zależności od wartości wskaźnika:
+             * < 18,5 –niedowaga,
+             * 18,5–24,99 –wartość prawidłowa,
+             * ≥ 25,0 –nadwaga.
+             * 
+             * b)Korzystając  z  Wikipedii  rozszerz  program,  tak  abywyświetlał  komentarz  według
+             * poszerzonej klasyfikacji zakresów wskaźnika BMI.
              */
 
-
-            Console.Write("Provide number a (bigger than zero): ");
-            var aNumber = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-            Console.Write("Provide number b: ");
-            var bNumber = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-            Console.Write("Provide number c: ");
-            var cNumber = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-
-            var calculatedDelta = DeltaCalculator(aNumber, bNumber, cNumber);
-            Console.WriteLine($"Calculated delta: {calculatedDelta}");
-            if (calculatedDelta > 0)
-            {
-                Console.WriteLine($"Results: {ZeroPlaces(calculatedDelta, aNumber, bNumber)}");
-            }
-            else
-            {
-                Console.WriteLine("Delta is less than zero so no zero places.");
-            }
-
+            Console.WriteLine("BMI calculator.");
+            Console.Write("Provide mass value in kg: ");
+            var massValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
+            Console.Write("Provide height value in m: ");
+            var heightValue = StringToDecimalParseAndDotReplace(Console.ReadLine());
+            var bmiValue = BmiCalculator(massValue, heightValue);
+            Console.WriteLine($"Calculated BMI: {bmiValue:0.##}");
+            Console.WriteLine($"BMI indicator: {BmiIndicator(bmiValue)}");
             Console.ReadKey();
         }
 
-        private static double DeltaCalculator(double aNumber, double bNumber, double cNumber)
+        private static double BmiCalculator(double massValue, double heightValue)
         {
-            var calculatedDelta = Math.Pow(bNumber, 2) - 4 * aNumber * cNumber;
-            return calculatedDelta;
+            var calculatedBmi = massValue / Math.Pow(heightValue, 2);
+            return calculatedBmi;
         }
 
-        private static string ZeroPlaces(double delta, double aNumber, double bNumber)
+        private static double StringToDecimalParseAndDotReplace(string toConvert)
         {
-            var xOne = (-bNumber - Math.Sqrt(delta)) / (2 * aNumber);
+            var dotReplaced = toConvert.Contains(".") ? toConvert.Replace(".", ",") : toConvert;
+            var convertedValue = double.Parse(dotReplaced);
+            return convertedValue;
+        }
 
-            var xTwo = (-bNumber + Math.Sqrt(delta)) / (2 * aNumber);
+        private static string BmiIndicator(double calculatedBmi)
+        {
+            var indicator = "";
+            if (calculatedBmi < 16.0)
+            {
+                indicator = "Wygłodzenie";
+            }
+            else if (calculatedBmi < 16.99)
+            {
+                indicator = "Wychudzenie";
+            }
+            else if (calculatedBmi < 18.49)
+            {
+                indicator = "Niedowaga";
+            }
+            else if (calculatedBmi < 24.99)
+            {
+                indicator = "Pożądana masa ciała";
+            }
+            else if (calculatedBmi < 29.99)
+            {
+                indicator = "Nadwaga";
+            }
+            else if (calculatedBmi < 34.99)
+            {
+                indicator = "Otyłość I stopnia";
+            }
+            else if (calculatedBmi < 39.99)
+            {
+                indicator = "Otyłość II stopnia (duża)";
+            }
+            else
+            {
+                indicator = "Otyłość III stopnia (chorobliwa)";
+            }
 
-            return $"x1: {xOne} and x2: {xTwo}";
+            return indicator;
         }
     }
 }
