755f62cd16db4bc3b0ad62a8396db8518a08736e
LivingBrain <realcookie@yahoo.com>
2020-06-09 21:19:43 +0200

    Zadanie 4.3

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -7,44 +7,88 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Napisz program kopiujący z danej tablicy liczb całkowitych tab1 do nowej tablicy tab2
-             * wyłącznie wartości dodatnie.Obie tablice mają być jednowymiarowe o rozmiarze równym 10
-             * (czyli  10-elemetowe).  Elementy  pierwszej  tablicy  (tab1) należy wpisać w trakcie deklaracji  
-             * tej tablicy.
+             * Napisz program wyświetlający informacje o wypełnionej przez użytkownika tablicy n-
+             * elementowej:
+             * wartość i numer pozycjinajwiększego elementu,
+             * wartość i numer pozycjinajmniejszego elementu
+             * średnia wartości wszystkich elementów tablicy
+             * liczba dodatnich elementów tablicy.
              */
 
-            Console.WriteLine("Comy positive numbers to table 2.");
-            var firstTable = new int[10];
-            var secondTable = new int[10];
+            Console.WriteLine("Table info.");
+            Console.Write("How big table?: ");
+            var table = new int[int.Parse(Console.ReadLine())];
             
-            for (int i = 0; i < firstTable.Length; i++)
+            for (int i = 0; i < table.Length; i++)
             {
                 Console.Write($"Provide number for table element no {i+1}: ");
-                firstTable[i] = int.Parse(Console.ReadLine());
+                table[i] = int.Parse(Console.ReadLine());
             }
-            PrintaTable(CopyPositiveNumbersToSecondTable(firstTable, secondTable));
+
+            Console.WriteLine(GetValueAndPositionOfBiggestElementInTable(table));
+            Console.WriteLine(GetValueAndPositionOfSmalestElementInTable(table));
+            Console.WriteLine(GetAverageOfAllTableElements(table));
+            Console.WriteLine(GetPositiveTableElementsCount(table));
 
             Console.ReadKey();
         }
 
-        private static int[] CopyPositiveNumbersToSecondTable(int[] firstTable, int[] secondTable)
+        private static string GetValueAndPositionOfBiggestElementInTable(int[] table)
         {
-            var tableIndex = 0;
-            foreach (var element in firstTable)
+            var biggestElementValue = 0;
+            var biggestElementPosition = 0;
+
+            for (int i = 0; i < table.Length; i++)
             {
-                if (element > 0) secondTable[tableIndex++] = element;
+                if (table[i] >= biggestElementValue)
+                {
+                    biggestElementValue = table[i];
+                    biggestElementPosition = i + 1;
+                }
             }
-            return secondTable;
+
+            return $"Table biggest number {biggestElementValue} at postion {biggestElementPosition}";
         }
 
-        private static void PrintaTable(int[] table)
+        private static string GetValueAndPositionOfSmalestElementInTable(int[] table)
         {
-            Console.WriteLine("Table begin.");
-            foreach (var item in table)
+            var smalestElementValue = 0;
+            var smalestElementPosition = 0;
+
+            for (int i = 0; i < table.Length; i++)
             {
-                Console.WriteLine(item);
+                if (table[i] <= smalestElementValue)
+                {
+                    smalestElementValue = table[i];
+                    smalestElementPosition = i + 1;
+                }
             }
-            Console.WriteLine("Table end.");
+
+            return $"Table biggest number {smalestElementValue} at postion {smalestElementPosition}";
         }
+
+        private static string GetAverageOfAllTableElements(int[] table)
+        {
+            var sum = 0;
+            foreach (var element in table)
+            {
+                sum += element;
+            }
+            decimal average = (decimal)sum / table.Length;
+
+            return $"Table elements averege is: {average}";
+        }
+
+        private static string GetPositiveTableElementsCount(int[] table)
+        {
+            var counter = 0;
+            foreach (var element in table)
+            {
+                if (element > 0) counter++;
+            }
+
+            return $"Table contains {counter} positive elements.";
+        }
+
     }
 }
