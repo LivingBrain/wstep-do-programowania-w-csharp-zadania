b3513081952d3551b0ca5195cf7a93d3fa0ec7a9
LivingBrain <realcookie@yahoo.com>
2020-06-16 16:17:07 +0200

    Zadanie 4.5

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -7,48 +7,51 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Napisz program, który podaje,ile jest liczb pierwszych w tablicy 100 elementowejtypu 
-             * int.Tablicę  należy  wypełnić  losowymi  wartościami. Wskazówka:  Poniższy  fragment
-             * programu pokazuje działanie klasy Random(która zawiera generator liczb pseudolosowych) –
-             * w pętli zostanie wyświetlonych100  liczbwybranych  losowo z  zakresu  1 –999(o  zakresie
-             * decydują argumenty podane w wywołaniu metody Next()). 
+             * Dana  jest n-elementowa  tablica  liczb  całkowitych tab1.  Napisz  program  kopiujący 
+             * wartości elementów tablicy tab1do tablicy tab2(o tym samym rozmiarze) z przesunięciem o 
+             * jedną pozycje. To znaczy, że element w tablicy źródłowej o indeksie 0 powinien znaleźć się w 
+             * tablicy  docelowej  pod  indeksem  1,  element  o  indeksie 1 ma być w tablicy docelowej pod 
+             * indeksem 2 itd. Element ostatni tablicy źródłowej ma być elementem o  indeksie  0 w  tablicy
+             * docelowej.
              */
 
             Random random = new Random();
 
-            Console.WriteLine("Table prime number.");
-            var table = new int[100];
-            
+            Console.WriteLine("Copy table and move values by one position.");
+            int[] table = new int[10];
+
             for (int i = 0; i < table.Length; i++)
             {
-                table[i] = random.Next(1, 999);
+                table[i] = random.Next(1, 99);
             }
 
-            Console.WriteLine($"Number of prime numbers in table: {TablePrimeNumberCount(table)}");
+            Console.WriteLine($"Generated table values: ");
+            PrintaTable(table);
+            Console.WriteLine($"Copied table values: ");
+            PrintaTable(CopyTable(table));
+
 
             Console.ReadKey();
         }
-        
-        private static int TablePrimeNumberCount(int[] table)
+
+        public static int[] CopyTable(int[] table)
         {
-            var primeNumberCounter = 0;
-            foreach (var number in table)
+            int[] secondTable = new int[table.Length];
+
+            secondTable[0] = table[table.Length - 1];
+            Array.Copy(table, 0, secondTable, 1, table.Length - 1);
+
+            return secondTable;
+        }
+
+
+        private static void PrintaTable(int[] table)
+        {
+            foreach (var item in table)
             {
-                var divisionWithRest = 0;
-
-                for (int i = 2; i < number; i++)
-                {
-                    if (number % i != 0) divisionWithRest++;
-                }
-
-                if (divisionWithRest == number - 2)
-                {
-                    primeNumberCounter++;
-                    Console.WriteLine(number);
-                }
+                Console.Write($"{item} ");
             }
-
-            return primeNumberCounter;
+            Console.WriteLine();
         }
     }
 }
