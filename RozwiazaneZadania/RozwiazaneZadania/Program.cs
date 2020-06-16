5bef18bc814be0c771ffe286a7f3326b800a68ef
LivingBrain <realcookie@yahoo.com>
2020-06-16 17:35:31 +0200

    Zadanie 4.7

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -7,45 +7,59 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Napisz  program,  który deklaruje  i  inicjalizuje  dwuwymiarową  tablicę liczb
-             * rzeczywistych o  rozmiarze 5 x 5. Program  ma  wyświetlić  elementy  tablicy  (wiersz  po 
-             * wierszu),  a  następnie wyświetlić  sumę elementów  znajdujących  się  na  głównej  przekątnej 
-             * tablicy(główna przekątna – od elementu o indeksach 0,0 do elementu o indeksach n,n)
+             * Napisz program, który dodaje dwie macierze o rozmiarze 2 x 3. Elementy macierzy 
+             * należy umieścićw tablicach dwuwymiarowych w trakcie deklaracji. Program ma wyświetlić
+             * macierz  wynikową.  Wskazówka:  Dodawanie  macierzy –macierz  wynikowa C zawiera 
+             * elementy,  które stanowią sumę elementów macierzy A  i  B o  odpowiednich  indeksach,  tzn.
+             * element  w  zerowym  wierszu  i  zerowej  kolumnie  macierzy  A  jest  dodawany  do  elementu  o
+             * tych samych indeksach macierzy B, element A [0,1]do B [0,1]...itd.
              */
 
-            Random random = new Random();
+            var random = new Random();
+            var xDimension = 2;
+            var yDimension = 3;
 
-            Console.WriteLine("Two dimension table.");
-            var table = new int[5, 5];
+            var firstMatrix = GenerateMatrix(random, xDimension, yDimension);
+            var secondMatrix = GenerateMatrix(random, xDimension, yDimension);
 
-
-            for (int i = 0; i < table.GetLength(0); i++)
-            {
-                for (int j = 0; j < table.GetLength(1); j++)
-                {
-                    table[i, j] = random.Next(1, 99);
-                }
-            }
-
-            Console.WriteLine($"Generated table values: ");
-            PrintaTwoDimensionTable(table);
-            Console.WriteLine($"Calculated sum: {SumValues(table)}");
+            Console.WriteLine("First matrix");
+            PrintaTwoDimensionTable(firstMatrix);
+            Console.WriteLine("Second matrix");
+            PrintaTwoDimensionTable(secondMatrix);
+            Console.WriteLine();
+            Console.WriteLine("Sum of two matrices");
+            PrintaTwoDimensionTable(AddTwoMatrixs(firstMatrix, secondMatrix));
 
             Console.ReadKey();
         }
 
-        public static int SumValues(int[,] table)
+        public static int[,] AddTwoMatrixs(int[,] firstMatrix, int[,] secondMatrix)
         {
-            int sum = 0;
-
-            for (int i = 0; i < table.GetLength(0); i++)
+            var thirdMatrix = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
+            for (int i = 0; i < thirdMatrix.GetLength(0); i++)
             {
-                sum += table[i, i];
+                for (int j = 0; j < thirdMatrix.GetLength(1); j++)
+                {
+                    thirdMatrix[i, j] = firstMatrix[i, j] + secondMatrix[i, j];
+                }
             }
 
-            return sum;
+            return thirdMatrix;
         }
 
+        private static int[,] GenerateMatrix(Random random, int xDimension, int yDimension)
+        {
+            var table = new int[xDimension, yDimension];
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                for (int j = 0; j < table.GetLength(1); j++)
+                {
+                    table[i, j] = random.Next(1, 100);
+                }
+            }
+
+            return table;
+        }
 
         private static void PrintaTwoDimensionTable(int[,] table)
         {
@@ -53,21 +67,10 @@ namespace RozwiazaneZadania
             {
                 for (int j = 0; j < table.GetLength(1); j++)
                 {
-                    if (i == j)
-                    {
-                        Console.BackgroundColor = ConsoleColor.White;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                    } else
-                    {
-                        Console.ResetColor();
-                    }
                     Console.Write("{0, 4}", table[i, j]);
-
                 }
                 Console.WriteLine();
             }
-
-            Console.ResetColor();
         }
     }
 }
