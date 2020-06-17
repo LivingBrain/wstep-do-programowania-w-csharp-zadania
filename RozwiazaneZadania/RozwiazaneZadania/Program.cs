a551beec5be580d551ac0bf4c3652d0997a7a653
LivingBrain <realcookie@yahoo.com>
2020-06-17 19:11:20 +0200

    Zadanie 4.15

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -9,35 +9,91 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * W danej firmie środki trwałe mają identyfikatory złożone z kilku liter, myślnika oraz 
-             * czterech  cyfr.  Te  cztery  cyfry  to  rok  zakupu  danego  środka  trwałego.  Przykładowe
-             * identyfikatory to: KOMG-2002, BH-2010. Napisz program, który deklaruje 5-cio elementową
-             * tablicę  typu stringdla  środków  trwałych,  którą  należy  zainicjalizować  przykładowymi
-             * identyfikatorami w czasie deklaracji. Program ma dla każdego środka trwałegopodać liczbę
-             * lat, jakie upłynęły od jego zakupu.
+             * Napisz program,  który  szyfruje  podany  przez  użytkownika  tekst  prostym  szyfrem 
+             * podstawieniowym zwanym  „gaderypoluki”.  Nazwa  pochodzi  od  jednego  z  najczęściej
+             * używanych kluczy GA-DE-RY-PO-LU-KI. Klucz ten zawiera pary znakowych zamienników
+             * umieszczonych między  myślnikami.  Litery,  których  nie  ma  w  kluczu  pozostawia  się  w 
+             * szyfrowanym tekście bez zmian. Przykładowo tekst „DRZEWO” po zaszyfrowaniu ma postać
+             * „EYZDWP”.
              */
 
-            var codesTable = new string[5] {
-                "KOMG-2002",
-                "BH-2010",
-                "TE-1999",
-                "USE-2018",
-                "KART-1972" };
+            var cipherKey = "GA-DE-RY-PO-LU-KI";
+            var alternativeCipherKey = "GADERYPOLUKI";
+            var originalText = "Litery, których nie ma w kluczu tekście bez zmian";
+            var cipheredText = "Iulcz tdn zgwkdyg ogyr zngipwrch zgmkdnnkiów";
 
-
-            foreach (var productCode in codesTable)
-            {
-                Console.WriteLine($"Product with code {productCode} is {HowOld(productCode)} years old.");
-            }
+            Console.WriteLine($"Original text: {originalText}");
+            Console.WriteLine($"Ciphered text: {CipherDecipher(originalText, cipherKey)}");
+            Console.WriteLine();
+            Console.WriteLine($"Original ciphered text: {cipheredText}");
+            Console.WriteLine($"Deciphered text: {CipherDecipher(cipheredText, cipherKey)}");
+            Console.WriteLine();
+            Console.WriteLine("*** Alternative solution ***");
+            Console.WriteLine($"Original text: {originalText}");
+            Console.WriteLine($"Ciphered text: {AlternativeCipherDecipher(originalText, alternativeCipherKey)}");
+            Console.WriteLine();
+            Console.WriteLine($"Original ciphered text: {cipheredText}");
+            Console.WriteLine($"Deciphered text: {AlternativeCipherDecipher(cipheredText, alternativeCipherKey)}");
 
             Console.ReadKey();
         }        
 
-        private static int HowOld(string productCode)
+        private static string CipherDecipher(string originalText, string cipherKey)
         {
-            var currentDate = int.Parse(DateTime.Now.ToString("yyyy"));
-            var yearOfProductPurchase = int.Parse(productCode.Split('-')[1]);
-            return currentDate - yearOfProductPurchase;
+            var convertedText = "";
+            var text = originalText.ToLower();
+            var cipherKeySplitList = cipherKey.ToLower().Split('-');
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var newChar = "";
+                for (int j = 0; j < cipherKeySplitList.Length; j++)
+                {
+                    if (cipherKeySplitList[j].Contains(text[i]))
+                    {
+                        newChar = cipherKeySplitList[j].Replace(text[i].ToString(), "");
+                        break;
+                    } 
+                }
+
+                if (string.IsNullOrEmpty(newChar))
+                {
+                    convertedText += Char.IsUpper(originalText[i]) ? text[i].ToString().ToUpper() : text[i].ToString(); 
+                }
+                else
+                {
+                    convertedText += Char.IsUpper(originalText[i]) ? newChar.ToUpper() : newChar; ;
+                }
+            }
+
+            return convertedText;
+        }
+
+        private static string AlternativeCipherDecipher(string originalText, string cipherKey)
+        {
+            var convertedText = "";
+            var text = originalText.ToLower();
+            var cipherKeyLower = cipherKey.ToLower();
+
+            foreach (var textChar in text)
+            {
+                var charIndexInKey = cipherKeyLower.IndexOf(textChar);
+
+                if (charIndexInKey % 2 == 0)
+                {
+                    convertedText += cipherKeyLower[charIndexInKey + 1];
+                }
+                else if (charIndexInKey == -1)
+                {
+                    convertedText += textChar;
+                }
+                else 
+                {
+                    convertedText += cipherKeyLower[charIndexInKey - 1];
+                }
+            }
+
+            return convertedText;
         }
 
     }
