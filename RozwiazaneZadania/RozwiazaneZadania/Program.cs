99b8087e2bce89ffc3ae0a6a3fa00c73f697f244
LivingBrain <realcookie@yahoo.com>
2020-06-26 16:03:16 +0200

    Zadanie 5.3

--- RozwiazaneZadania/RozwiazaneZadania/Program.cs
+++ RozwiazaneZadania/RozwiazaneZadania/Program.cs
@@ -7,26 +7,38 @@ namespace RozwiazaneZadania
         static void Main(string[] args)
         {
             /*
-             * Napisz  program  wczytujący  3  liczby  rzeczywiste a, b, x,  a  następnie  wywołujący
-             * metodę,  która  sprawdza,  czy  liczba x należy  do  przedziału  obustronnie  otwartego (a,  b).
-             * Metoda sprawdzająca ma zwrócić wartość logiczną, którą należy zinterpretować w metodzie
-             * Main() z podaniem stosownego komunikatu. 
+             * Napisz program, który ma znaleźć współrzędne punktu po przesunięciu o dany wektor. 
+             * W   metodzie Main()wczytaj   od  użytkownika  współrzędne  punktu Aoraz   zadeklaruj 
+             * współrzędne wektorawek[3, 2], a następnie wywołaj metodę o nazwie Przesun(), która ma 
+             * przesunąć  punkt A o  wektor wek(dodać  odpowiednie  współrzędne).  Współrzędne  punktu
+             * (jako  dwie  zmienne  typu double)  mają zostać przesłane do tej  metody  przez referencję, a 
+             * współrzędne  wektora  (także  jako  dwie  zmienne  typu double)  przez  wartość.  Metoda 
+             * Przesun()ma nic nie zwracać (void), aktualne współrzędne punktu mają być pamiętane dzięki
+             * użyciu  argumentów  przesyłanych przez  referencje.  Program  ma  wyświetlić  współrzędne
+             * punktu po przesunięciu  o wektor wek. Przykładowo, gdyby użytkownik podał początkowe 
+             * współrzędne  punktu A(2,  1),  to  wówczas  program  znajdzie  położenie  punktu A po 
+             * przesunięciu w miejscu o współrzędnych (5, 3) (czyli 2+3, 1+2).
              */
+            var aPoint = new int[2];
+            var vector = new int[2] { 3, 2 };
 
-            Console.Write("Provide value a: ");
+            Console.Write("Provide first value of a point: ");
             int aValue = int.Parse(Console.ReadLine());
-            Console.Write("Provide value b: ");
+            Console.Write("Provide second value of a point: ");
             int bValue = int.Parse(Console.ReadLine());
-            Console.Write("Provide value x: ");
-            int xValue = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine($"Is x between a and b?: {CheckIfNumberIsBetween(aValue, bValue, xValue)}");
+            aPoint[0] = aValue;
+            aPoint[1] = bValue;
+
+            Przesun(ref aPoint, vector);
+
+            Console.WriteLine($"A point values: [{aPoint[0]}, {aPoint[1]}] ");
             Console.ReadKey();
         }
 
-        private static bool CheckIfNumberIsBetween(int aValue, int bValue, int xValue)
+        private static void Przesun(ref int[] aPoint, int[] vector)
         {
-            return aValue < xValue && xValue < bValue ? true : false;
+            aPoint[0] += vector[0];
+            aPoint[1] += vector[1];
         }
     }
 }
